1359aa319246eaecd428cd2e830cbe45d5b009d8
yuzhangbin <yuzhangbin@souche.com>
2024-01-15 22:28:51 +0800

    feat:新增类

---
+++ springboot-nextjs-demo-web/src/main/java/com/gcbjs/demo/mappers/TicketInfoMapper.java
@@ -0,0 +1,22 @@
+package com.gcbjs.demo.mappers;
+
+import com.gcbjs.demo.mappers.model.TicketInfo;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+  *@ClassName TicketRepository
+  *@Description  工单仓储类
+  *@Author yuzhangbin
+  *@Date 2024/1/15 16:56
+  *@Version 1.0
+**/
+@Repository
+public interface TicketInfoMapper {
+
+    int insert(TicketInfo ticketInfo);
+
+    int update(TicketInfo ticketInfo);
+
+    TicketInfo findByTicketId(@Param("ticketId") Long ticketId);
+}
