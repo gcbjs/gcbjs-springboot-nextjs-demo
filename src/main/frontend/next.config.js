9772c157152bd79ba3c149a64a6c9836ebf86781
yuzhangbin <yuzhangbin@souche.com>
2024-01-19 00:10:43 +0800

    feat:重构结构

--- src/main/frontend/next.config.js
+++ src/main/frontend/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ["antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table"],
   images: { unoptimized: true }
 }
 
