4658713b92138ccacc549e5832164187b4695000
yuzhangbin <yuzhangbin@souche.com>
2024-01-22 11:30:57 +0800

    feat:完成4-1

---
+++ src/main/frontend/app/api/route.js
@@ -0,0 +1,9 @@
+import {NextResponse} from "next/server";
+
+export async function GET() {
+    const res = await fetch('http://localhost:8080/api/getUserQueue')
+    console.log("res",res)
+    const data = await  res.json()
+
+    return NextResponse.json({data})
+}
--- src/main/frontend/app/page.js
+++ src/main/frontend/app/page.js
@@ -1,8 +1,12 @@
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import {GET} from "./api/route";
 
 export default function Home() {
+
+  const data = GET();
   return (
-    <h1>Hello,App!</h1>
+    <h1>{data}</h1>
   );
 }
