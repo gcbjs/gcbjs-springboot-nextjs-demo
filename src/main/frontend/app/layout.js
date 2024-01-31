5a6b91f70454795651e317646ad47916fd00c81a
yuzhangbin <yuzhangbin@souche.com>
2024-01-31 10:03:03 +0800

    feat:新增权限

--- src/main/frontend/app/layout.js
+++ src/main/frontend/app/layout.js
@@ -1,10 +1,12 @@
-import Sidebar from "../components/Sidebar";
+import Sidebar from "@/components/Sidebar";
 import './style.css'
+import Header from "@/components/Header";
 
 export default function RootLayout({ children}) {
   return (
     <html lang="en">
       <body className="container">
+      <Header />
       <div className="main">
           <Sidebar/>
           <section className="col note-viewer" >{children}</section>
